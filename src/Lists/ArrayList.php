bf8c3f09e4142d8c6bede1a89fdf52675849caa1
Aure6 <aurelien7112001@gmail.com>
2023-10-25 11:52:07 +0200

    linkedlist

--- src/Lists/ArrayList.php
+++ src/Lists/ArrayList.php
@@ -18,23 +18,81 @@ class ArrayList implements ListInterface
         return json_encode($this->elements, JSON_PRETTY_PRINT);
     }
 
-    public function push(mixed $element = null): void {}
+    public function push(mixed $element = null): void {
+        /* $this->elements[] = $element; */
+        if(empty($this->elements)){
+            //array_push($this->elements, $element);
+            $this->elements[0]=$element;
+        } elseif (gettype($this->elements[0]) === gettype($element)) {
+            array_push($this->elements, $element);
+        } else {
+            throw new \InvalidArgumentException("it should return an error if adding an element of a different type");
+        }
+    }
 
-    public function get(int $index): mixed {}
+    public function get(int $index): mixed {
+        if (array_key_exists($index,$this->elements)){
+            return $this->elements[$index];
+        } else {
+            throw new \OutOfBoundsException("Index '$index' is out of bounds.");
+        }
+    }
 
-    public function set(int $index, mixed $element): void {}
+    public function set(int $index, mixed $element): void {
+        if ($index < count($this->elements)) {
+            //$this->elements[$index] = $element;
+            array_splice($this->elements, $index, 0, $element);
+        } else {
+            //it should throw exception when set index not found
+            throw new \InvalidArgumentException('Element cannot be null');
+            //throw new \InvalidArgumentException('Index already exists');
+        }
 
-    public function clear(): void {}
+        /* if (!is_null($element)) {
+            throw new \InvalidArgumentException('Element cannot be null');
+        } */
+        
+    }
 
-    public function includes(mixed $element): bool {}
+    public function clear(): void {
+        $this->elements = [];
+    }
 
-    public function isEmpty(): bool {}
+    public function includes(mixed $element): bool {
+        if (in_array($element, $this->elements)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-    public function indexOf(mixed $element): int {}
+    public function isEmpty(): bool {
+        // TODO: vérifier que c'est un tableau et après utiliser empty
+        return empty($this->elements);
+    }
 
-    public function remove(int $index): void {}
+    public function indexOf(mixed $element): int {
+        if (in_array($element, $this->elements)){
+            return array_search($element, $this->elements);
+        } else {
+            throw new \InvalidArgumentException('Index of element not found');
+        }
+    }
 
-    public function size(): int {}
+    public function remove(int $index): void {
+        if (array_key_exists($index, $this->elements)){
+            array_splice($this->elements, $index, 1);
+            //unset($this->elements[$index]);
+        } else {
+            throw new \InvalidArgumentException('Index not found');
+        }
+    }
 
-    public function toArray(): array {}
+    public function size(): int {
+        return count($this->elements);
+    }
+
+    public function toArray(): array {
+        return $this->elements;
+    }
 }
--- src/Lists/LinkedList.php
+++ src/Lists/LinkedList.php
@@ -22,23 +22,106 @@ class LinkedList implements ListInterface
         return json_encode($this->toArray(), JSON_PRETTY_PRINT);
     }
 
-    public function push(mixed $element): void {}
+    public function push(mixed $element): void {
+        // Créez un nouveau nœud avec l'élément fourni
+        $newNode = new Node($element);
 
-    public function get(int $index): mixed {}
+        // Si la liste est vide
+        if ($this->size == 0) {
+            $this->head->setNext($newNode);
+        } else {
+            // Parcourez la liste jusqu'au dernier nœud
+            $currentNode = $this->head;
+            while ($currentNode->getNext() !== null) {
+                $currentNode = $currentNode->getNext();
+            }
+            // Ajoutez le nouveau nœud à la fin de la liste
+            $currentNode->setNext($newNode);
+        }
 
-    public function set(int $index, mixed $element): void {}
+        // Incrémentez la taille de la liste
+        $this->size++;
+    }
 
-    public function clear(): void {}
+    public function get(int $index): mixed {
+        // Vérifiez que l'index est valide
+        if ($index < 0 || $index >= $this->size) {
+            throw new \OutOfRangeException('Index is out of range');
+        }
 
-    public function includes(mixed $element): bool {}
+        // Commencez au premier nœud (head) 
+        $currentNode = $this->head->getNext(); // car la tête ne contient pas de donnée utile
+        $currentIndex = 0;
+
+        // Parcourez la liste jusqu'à atteindre l'indice demandé
+        while ($currentIndex < $index && $currentNode !== null) {
+            $currentNode = $currentNode->getNext();
+            $currentIndex++;
+        }
+
+        // Vérifiez que le nœud actuel n'est pas null avant d'essayer d'appeler une méthode dessus
+        if ($currentNode === null) {
+            throw new \RuntimeException('Unexpected error: Node is null.');
+        }
+
+        // Une fois que vous avez atteint l'indice demandé, retournez l'élément du nœud
+        return $currentNode->getElement();
+    }
+
+    public function set(int $index, mixed $element): void {
+
+        /* // Initialisez une variable pour stocker le nœud actuel et le décalage
+            $nodeCurrent = $this->head; */
+    }
+
+    public function clear(): void {
+        // Réinitialisez le head à un nouveau nœud avec une valeur null
+        $this->head = new Node(null);
+
+        // Réinitialisez la taille à 0
+        $this->size = 0;
+    }
+
+    public function includes(mixed $element): bool {
+        // Commencez par vérifier si le head a une valeur null ou non
+    }
 
     public function isEmpty(): bool {}
 
     public function indexOf(mixed $element): int {}
 
-    public function remove(int $index): void {}
+    public function remove(int $index): void {
+        // Vérifiez que l'index est valide
+        if ($index < 0 || $index >= $this->size) {
+            throw new \OutOfRangeException('Index is out of range');
+        }
 
-    public function size(): int {}
+        $previousNode = $this->head;
+        $currentNode = $this->head->getNext();
+        $currentIndex = 0;
+
+        // Parcourez la liste jusqu'à atteindre l'indice demandé
+        while ($currentIndex < $index && $currentNode !== null) {
+            $previousNode = $currentNode;
+            $currentNode = $currentNode->getNext();
+            $currentIndex++;
+        }
+
+        // Si le nœud actuel est nul, cela signifie que nous avons un problème (mais cela ne devrait pas arriver car nous avons vérifié l'indice au début)
+        if ($currentNode === null) {
+            throw new \RuntimeException('Unexpected error: Node is null.');
+        }
+
+        // Ajustez la référence du nœud précédent pour qu'elle pointe vers le nœud après le nœud actuel
+        $previousNode->setNext($currentNode->getNext());
+
+        // Decrement the size of the list
+        $this->size--;
+    }
+
+    public function size(): int {
+        return $this->size;
+    }
 
     public function toArray(): array {}
 }
